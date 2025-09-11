c60e60e0e4cbe5abd6d7026fc3718ecace111c45
Gipson Pulla <pullagipson@gmail.com>
2025-09-11 14:09:31 +0530

    removing the comments

--- terraform_s3/main.tf
+++ terraform_s3/main.tf
@@ -1,34 +1,3 @@
-/*
-resource "aws_s3_bucket" "my-bucket" {
-  bucket = "my-tf-test-bucketgipsonp"
-
-  tags = {
-    Name        = "My bucket"
-    Environment = "Dev"
-  }
-}*/
-
-/*
-resource "aws_s3_bucket" "my-bucket" {
-    bucket = "gips-my-bucket"
-    #acl = private
-    versioning {
-      enabled = true
-    }
-}
-*/
-
-/*
-resource "aws_s3_bucket_server_side_encryption_configuration" "example" {
-    bucket = aws_s3_bucket.my-bucket.id
-    
-    rule {
-        apply_server_side_encryption_by_default {
-            sse_algorithm     = "AES256"
-            }
-        }
-}*/
-
 
 resource "aws_s3_bucket" "terraform_state" {
   bucket = "gips-my-bucket1"
